cb2929b1343b9e5f0ce70982fe711dcb3f3ebf7e
vinhnguyenplus <43105314+vinhnguyenplus@users.noreply.github.com>
2026-03-07 13:09:20 +0700

    Init source

---
+++ Web/src/views/home/widgets/components/about.vue
@@ -0,0 +1,27 @@
+<template>
+	<card-pro title="关于项目" prefix-icon="ele-QuestionFilled" shadow="hover">
+        <p>
+			Information!
+		</p>
+    </card-pro>
+</template>
+
+<script lang="ts">
+export default {
+	title: '关于项目',
+	icon: 'ele-QuestionFilled',
+	description: '点个星星支持一下',
+};
+</script>
+<script setup lang="ts" name="about">
+import CardPro from '/@/components/CardPro/index.vue';
+</script>
+
+<style scoped>
+.item-background p {
+	color: #999;
+	margin-top: 10px;
+	line-height: 1.8;
+	/* height: 100px; */
+}
+</style>
